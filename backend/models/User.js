ee22b83e244a6e702189f75abc6159df6701a4bf
Harsh252-dot <harshmaharshi571@gmail.com>
2025-03-19 20:10:06 +0530

    Hello

---
+++ backend/models/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+});
+
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", UserSchema);
